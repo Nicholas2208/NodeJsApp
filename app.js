8df3bcb100f605ee49c7c1fd997bb909cb2c2ffb
Nicholas2208 <nikolaj.belyakov.84@gmail.com>
2022-10-13 16:26:56 +0300

    make it better

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var path = require("path");
 
-var routes = require("./routes");
+//var routes = require("./routes");
 
 var app = express();
 
@@ -9,7 +9,8 @@ app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(routes);
+app.use("/", require("./routes/web"));
+app.use("/api", require("./routes/api"));
 
 app.use(express.static('public'));
 
